a0aea53b08be6d35fb425a4ea6b388ecd2913741
majamus <majamus@o2.pl>
2020-02-10 18:08:14 +0100

    dodano wersja czarno-biała dodano zaznacz linki

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,27 @@ document.getElementById("fonta").onclick = function() {changeFont(1)};
 document.getElementById("fontaa").onclick = function() {changeFont(2)};
 document.getElementById("fontaaa").onclick = function() {changeFont(3)};
 document.getElementById("contrast").onclick = function() {changeContrast()};
+document.getElementById("showborder").onclick = function(){markLinks()};
 
+var linkmarked = false;
+
+function markLinks()
+{
+	
+	var links = document.getElementsByTagName("a");
+	if(linkmarked == false){
+		for (var i = 0 ; i < links.length ; ++i)
+			links[i].style.border = "solid 5px blue";
+		
+		linkmarked = true;
+	}
+	else{
+		for (var i = 0 ; i < links.length ; ++i)
+			links[i].style.border = "";
+		
+		linkmarked = false;
+	}
+}
 function showMenu() {
 	var x = document.getElementById("leftnav");
 	if (x.style.display === "none") 
@@ -14,6 +34,7 @@ function showMenu() {
 		x.style.display = "none";
 	}
 }
+
 
 function changeFont(mod)
 {
@@ -41,14 +62,17 @@ function changeContrast(){
 	  // use the native .trim() if it exists
 	  //   otherwise use a regular expression  
 	htmlstring = (htmlstring.trim) ? htmlstring.trim() : htmlstring.replace(/^\s+/,'');
-	if(htmlstring == 'Kontrast') {
+	if(htmlstring == 'Wersja czarno-żółta') {
 		newlink.setAttribute("href", "assets/css/morecontrast.css");
-		element.innerHTML="Normalny kontrast";
+		element.innerHTML="Wersja czarno-biała";
 	}
-	
+	else if(htmlstring == 'Wersja czarno-biała') {
+		newlink.setAttribute("href", "assets/css/blackwhite.css");
+		element.innerHTML="Wersja zwykła";
+	}
 	else{
 		newlink.setAttribute("href", "assets/css/lesscontrast.css");
-		element.innerHTML='Kontrast';
+		element.innerHTML='Wersja czarno-żółta';
 	}
 	
 	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
