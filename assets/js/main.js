47b47a39ced94cb0e8030e8f468115170c73e422
majamus <majamus@o2.pl>
2020-02-12 19:06:43 +0100

    przerobiony nagłówek

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ document.getElementById("menubutton").onclick = function() {showMenu()};
 document.getElementById("fonta").onclick = function() {changeFont(1)};
 document.getElementById("fontaa").onclick = function() {changeFont(2)};
 document.getElementById("fontaaa").onclick = function() {changeFont(3)};
-document.getElementById("contrast").onclick = function() {changeContrast()};
+document.getElementById("contrast1").onclick = function() {changeContrast(1)};
+document.getElementById("contrast2").onclick = function() {changeContrast(2)};
+document.getElementById("contrast3").onclick = function() {changeContrast(3)};
 document.getElementById("showborder").onclick = function(){markLinks()};
 
 var linkmarked = false;
@@ -13,7 +15,7 @@ function markLinks()
 	var links = document.getElementsByTagName("a");
 	if(linkmarked == false){
 		for (var i = 0 ; i < links.length ; ++i)
-			links[i].style.border = "solid 5px blue";
+			links[i].style.border = "solid 2px blue";
 		
 		linkmarked = true;
 	}
@@ -42,6 +44,7 @@ function changeFont(mod)
 	var newlink = document.createElement("link");
     newlink.setAttribute("rel", "stylesheet");
     newlink.setAttribute("type", "text/css");
+	
 	if(mod == 1) newlink.setAttribute("href", "assets/css/fonta.css");
 	if(mod == 2) newlink.setAttribute("href", "assets/css/fontaa.css");
 	if(mod == 3) newlink.setAttribute("href", "assets/css/fontaaa.css");
@@ -49,31 +52,17 @@ function changeFont(mod)
 	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
 
-function changeContrast(){
+function changeContrast(mod){
 	
 	var oldlink = document.getElementsByTagName("link").item(1);
 	var newlink = document.createElement("link");
 	newlink.setAttribute("rel", "stylesheet");
 	newlink.setAttribute("type", "text/css");
-		
-	var element = document.getElementById("contrast");
-	var htmlstring = element.innerHTML;
-
-	  // use the native .trim() if it exists
-	  //   otherwise use a regular expression  
-	htmlstring = (htmlstring.trim) ? htmlstring.trim() : htmlstring.replace(/^\s+/,'');
-	if(htmlstring == 'Wersja czarno-żółta') {
-		newlink.setAttribute("href", "assets/css/morecontrast.css");
-		element.innerHTML="Wersja czarno-biała";
-	}
-	else if(htmlstring == 'Wersja czarno-biała') {
-		newlink.setAttribute("href", "assets/css/blackwhite.css");
-		element.innerHTML="Wersja zwykła";
-	}
-	else{
-		newlink.setAttribute("href", "assets/css/lesscontrast.css");
-		element.innerHTML='Wersja czarno-żółta';
-	}
+	
+	if(mod == 1) newlink.setAttribute("href", "assets/css/lesscontrast.css");
+	if(mod == 2) newlink.setAttribute("href", "assets/css/morecontrast.css");
+	if(mod == 3) newlink.setAttribute("href", "assets/css/blackwhite.css");
+	
 	
 	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
